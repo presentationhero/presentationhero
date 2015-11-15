e47194f1f138d89784b5376ef3902654378d6d9a
Marco <marco.gracia@cupenya.com>
2015-11-15 14:29:30 +0100

    add timer to start

--- Assets/start.js
+++ Assets/start.js
@@ -1,5 +1,28 @@
 ﻿#pragma strict
 
+var timer : int = 0;
+var timerStatus : boolean = false;
+
+function Start () {
+	InvokeRepeating( "Decrease", 0, 1 );
+}
+
+function Decrease () {
+
+	if (timerStatus) {
+		timer = timer + 1;
+	}
+	if (timer >= 3) {
+		Application.LoadLevel("walk_in");
+	}
+}
+
 public function OnPointerEnter(eventData) {
-    Application.LoadLevel("walk_in");
+	timer = 0;
+    timerStatus = true;
+}
+
+public function OnPointerExit(eventData) {
+	timer = 0;
+    timerStatus = false;
 }
