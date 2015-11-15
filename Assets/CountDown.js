89263523a57068cbd27c181a4e2100fbfad62266
george <george.karmoiris@cupenya.com>
2015-11-15 12:42:45 +0100

    countDown script

---
+++ Assets/CountDown.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+private var startTime : int = 4;
+private var countDown : int = 0;
+
+function Start () {
+  countDown = startTime;
+  InvokeRepeating( "Decrease", 1.0, 1.0 );
+}
+
+function Update () {
+
+}
+
+function Decrease () {
+	countDown = countDown - 1;
+	gameObject.GetComponent( UI.Text ).text = countDown.ToString();
+
+	if( countDown == 0 ) {
+		Debug.Log("BOOOO");
+	}
+}
